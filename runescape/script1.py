5d4743192da366f4e1d55aad7909c49ed75e0ef6
Dickon Fell <62467516+dickonfell@users.noreply.github.com>
2021-08-13 12:39:55 +0100

    Updated file and function descriptions, changed click() function "side" parameter to accept the strings "left"/"right" as opposed to the integers 1/2. TODO: if statement at line 49 can be condensed using setattr(), do it later

--- runescape/script1.py
+++ runescape/script1.py
@@ -2,15 +2,20 @@
 """
 This is a bot for playing Runescape. Specifically, when one has an amount of cake and chocolate in their bank,
 this bot will do the dirty work of combining the two to make chocolate cake, which one can then sell on the
-Grand Exchange for a tasty profit. It does this by controlling the movement and clicks of the mouse,
-which conveniently is all the controls used in Runescape.
+Grand Exchange for a tasty profit. It does this by controlling the movement and clicks of the mouse
+which, conveniently, is all that is needed to play the game.
+
+Disclaimer: The functions in this script do not work unless the following setup is completed.
 
 Setup:
-Cake in bank slot 1
-Chocolate in bank slot 2
-Empty Inventory
-Stand on the west side of the GE directly opposite the northernmost banker
-Zoom all the way in and reset camera angle by clicking the compass
+
+- Cake in bank slot 1
+- Chocolate in bank slot 2
+- 'Take x' set to 14 in bank menu
+- Empty Inventory
+- Stand on the west side of the GE directly opposite the northernmost banker
+- Zoom all the way in and reset camera angle by clicking the compass
+
 (there is no simple way around having this setup, unless you find the idea of coding an AI to navigate around
 all of these unknowns simple...)
 """
@@ -31,11 +36,9 @@ mouse = Controller()
 # distance across 'bank banker button is 90
 
 def click(location, xdim, ydim, side):
-    # Moves mouse to a location and left clicks it.
-    # dim defines the range in which randomness is generated,
-    # corresponds to the size of the object being clicked.
-
-    # side 1 = left click, side 2 = right click
+    # Moves cursor to a random location within 
+    # a specified area of a chosen point
+    # and left or right clicks
 
     r1 = random.randint(-xdim, xdim)
     r2 = random.randint(-ydim, ydim)
@@ -43,9 +46,9 @@ def click(location, xdim, ydim, side):
 
     mouse.position = tuple(map(lambda i, j: i+j, location, r))  # move to location
     time.sleep(0.25 + (r1 / 1000))
-    if side == 1:
+    if side == "left":
         mouse.click(Button.left, 1)  # left click
-    elif side == 2:
+    elif side == "right":
         mouse.click(Button.right, 1)  # right click
     time.sleep(0.5 + (r2 / 1000))
 
@@ -54,9 +57,9 @@ def click(location, xdim, ydim, side):
 # (1043, 422), (1100, 892)
 
 def enterbank():
-    # This function opens the bank when the player has completed the setup above.
+    # Enters the bank menu
 
-    click((1075, 650), 10, 25, 1)  # left clicks the board in front of the banker, this opens the bank
+    click((1075, 650), 10, 25, "left")  # left clicks the board in front of the banker, this opens the bank
 
     return
 
@@ -71,16 +74,14 @@ def enterbank():
 # (1067, 78) bottom right corner of close bank
 
 def grabcakerecipe():
-    # With an open bank on screen, this function takes out the ingredients for chocolate cake, those being
-    # cake and chocolate bars, from the first two bank slots.
-    # Each click puts 14 cake/chocolate in inventory with take x selected, x set to 14,
-    # this fills the inventory each time.
+    # When in the bank menu: takes 14 cake and chocolate bars each (the ingredients for chocolate cake)
+    # from the first and second bank slots respectively, and exits the bank
 
-    click((661, 147), 5, 5, 1)  # left click bank slot 1
+    click((661, 147), 5, 5, "left")  # left click bank slot 1
 
-    click((711, 145), 5, 5, 1)  # left click bank slot 2
+    click((711, 145), 5, 5, "left")  # left click bank slot 2
 
-    click((1060, 70), 5, 5, 1)  # left click 'close bank'
+    click((1060, 70), 5, 5, "left")  # left click 'close bank'
 
     return
 
@@ -94,29 +95,30 @@ def grabcakerecipe():
 # (1685, 820), (1771, 820) 'use cake' button dimensions
 
 def makecake():
-    # This function takes an inventory full of 14 cake and chocolate bars each, and makes them into chocolate cakes.
+    # With an inventory containing 14 cake in the top slots and 14 chocolate in the bottom,
+    # converts those ingredients into 14 chocolate cakes.
 
     r1 = random.randint(-40, 40)
 
-    click((1735, 774), 5, 5, 2)  # right click inventory slot 1
+    click((1735, 774), 5, 5, "right")  # right click inventory slot 1
 
     p = random.choice([-1, 1])  # randomly chooses -1 or 1
     shift = (r1, 40 + p)
     mouse.position = tuple(map(lambda i, j: i + j, mouse.position, shift))  # move to 'use cake'
-    click(mouse.position, 0, 0, 1)  # left click 'use cake'
+    click(mouse.position, 0, 0, "left")  # left click 'use cake'
 
-    click((1821, 882), 5, 5, 1)  # left click inventory slot 15
+    click((1821, 882), 5, 5, "left")  # left click inventory slot 15
     time.sleep(0.5)  # gives time for 'make cake' button to appear
 
-    click((260, 970), 40, 30, 1)  # left click 'make cake' button
+    click((260, 970), 40, 30, "left")  # left click 'make cake' button
     time.sleep(17.5)  # gives time for cake to be made
 
     return
 
 
 def play():
-    # This function defines the loop for the whole script, combining the elements above so that the bot makes as many
-    # chocolate cakes as you need, and then stops.
+    # The loop for the entire cake-making process, combining the functions above in order
+    # to automatically create a specified number of chocolate cakes.
 
     k = int(input('How many cakes are you making today? '))  # defines how many times to loop
     k = math.floor(k/14)
@@ -140,9 +142,10 @@ def play():
         time.sleep(0.5 + r1)
 
         # this deposits the made chocolate cake into the bank
-        click((1735, 774), 5, 5, 1)  # left click inventory slot 1
+        click((1735, 774), 5, 5, "left")  # left click inventory slot 1
 
     return
 
-play()
+if __name__ == "__main__":
+    play()
 
